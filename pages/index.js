1279b19a5639908562fb8890dc86aae38263dacf
Alexander Stratmoen <hello@alexanderstratmoen.ca>
2024-03-03 19:54:46 -0500

    Fixed heading

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Index(props) {
             <div>
               <h1>{title}</h1>
               <p>
-                <strong>Founder & Stealth and CE Student @ UW
+                <strong>Founder @ Stealth & CE @ UW
                 </strong><br />
                 {description}
               </p>
